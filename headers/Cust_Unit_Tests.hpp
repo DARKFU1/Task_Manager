67ccaaffa6faf3ef53eb7ae0159d2eec4b9eb712
Andrew-f <d_a_r_k_f_u_l@mail.ru>
2023-03-07 15:47:58 +0300

    Slight changes to Unit Tests

--- headers/Cust_Unit_Tests.hpp
+++ headers/Cust_Unit_Tests.hpp
@@ -6,12 +6,12 @@
 // For painting output in beautiful green "OK" and red "FAIL"
 
 static bool __result = false;
-static char* __name_of_test = new char[256];
+static char* __name_of_test;
 
 #define __PRINT_RESULT(result) if(result) { printf("[  %sOK%s  ] ", GRN, WHT); } else { printf("[ %sFAIL%s ] ", RED, WHT); } printf("%s\n", __name_of_test);
 #define PRINT_RESULT() __PRINT_RESULT(__result);
 
-#define TEST(name) strcpy(__name_of_test, name); __result = [&]() -> bool
+#define TEST(name) __name_of_test = name; __result = [&]() -> bool
 #define ENDTEST() (); PRINT_RESULT();
 
 #endif
